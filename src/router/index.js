3fad34f5de75382f0f2b5d5d23d6aab5a19a52c1
zhoujingyidev <1540640311@qq.com>
2019-07-11 20:51:07 +0800

    首页布局

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    { path: '/', name: 'home', component: Home }
   ]
 })
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <el-container class="home-container">
+    <el-aside class="my-aside" width="200px">
+      <div class="logo"></div>
+      <el-menu
+        default-active="1"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        style="border-right:none"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">首 页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-edit-outline"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-s-comment"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header class="my-header">
+        <span class="el-icon-s-fold"></span>
+        <span class="text">江苏传智播客教育科技有限公司</span>
+        <el-dropdown style="float:right">
+          <span class="el-dropdown-link">
+            <img
+              style="vertical-align:middle"
+              width="30"
+              height="30"
+              src="../../assets/images/avatar.jpg"
+              alt
+            />
+            <b style="vertical-align:middle;padding-left:5px">黑马小哥</b>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang='less'>
+.home-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .my-aside {
+    background: #002033;
+    .logo {
+      width: 100%;
+      height: 60px;
+      background: #002840 url(../../assets/images/logo_admin.png) no-repeat
+        center / 140px auto;
+    }
+  }
+  .my-header {
+    line-height: 60px;
+    border-bottom: 1px solid #ddd;
+    .el-icon-s-fold {
+      font-size: 26px;
+      vertical-align: middle;
+      margin-right: 5px;
+    }
+    .text {
+        vertical-align: middle;
+    }
+  }
+}
+</style>
